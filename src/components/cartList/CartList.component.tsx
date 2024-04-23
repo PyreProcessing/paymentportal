1290b777d757c46594d3fdca59e0cb071b4ae2a3
Yiyang Lu <ste900811@gmail.com>
2024-04-23 09:39:25 -0400

    Add the tax calculation

--- src/components/cartList/CartList.component.tsx
+++ src/components/cartList/CartList.component.tsx
@@ -8,7 +8,8 @@ import InventoryType from "@/types/InventoryType";
 
 const CartList = () => {
   const { cart, removeProductFromCart } = useCartStore();
-  console.log(cart)
+  function tax(price: any, taxRate: any) { return Number((price * taxRate / 100).toFixed(2)); }
+
   return (
     <div className={styles.container}>
       <div className={styles.contentContainer}>
@@ -68,8 +69,8 @@ const CartList = () => {
       {/* Tax container */}
       <div className={styles.shippingPriceContainer}>
         <h4>Tax</h4>
-        <span>{cart.reduce((acc, curr) => acc + curr.product.tax, 0)}%</span>
-        <span>${cart.reduce((acc, curr) => acc + (curr.product.price * curr.product.tax/100).toFixed(2) * curr.quantity, 0).toFixed(2)}</span>
+        <span>{cart[0]?.product.tax}%</span>
+        <span>${tax(cart[0]?.product.price, cart[0]?.product.tax)}</span>
       </div>
 
       {/* shipping price container */}
@@ -81,7 +82,7 @@ const CartList = () => {
       {/* total price container */}
       <div className={styles.totalPriceContainer}>
         <h4>Total</h4>
-        <span>${cart.reduce((acc, curr) => acc + (curr.product.price * (1 + curr.product.tax/100)).toFixed(2) * curr.quantity, 0).toFixed(2)}</span>
+        <span>${cart.reduce((acc, curr) => acc + (curr.product.price + tax(curr.product.price, curr.product.tax)) * curr.quantity, 0)}</span>
       </div>
     </div>
   );
